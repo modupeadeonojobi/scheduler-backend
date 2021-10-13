44cfd5f7f5900f6e7d9924abeaf872ce9010d36c
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-10-13 20:57:59 +0100

    Connect to database

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const http = require('http');
+const mongoose = require("mongoose");
 require('dotenv').config();
+const User = require('./models/user');
 
 
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('The port is working');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('The port is working');
 });
 
 
-// LISTEN
+
+// Database connection
+const dbURI = 'mongodb://localhost:27017/scheduler';
 const { PORT, HOST } = process.env;
-server.listen(PORT, HOST, () => {
-  console.log(`Listening for requests on port ${PORT}`);
-});
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    server.listen(PORT, HOST);
+    console.log(`Listening for requests on port ${PORT}`);
+    console.log('Connected to database')
+  })
+  .catch(error => console.log(error));
