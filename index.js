7d33eab58a1d6600a8c28b1285da308817cb8697
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2021-10-19 06:31:38 +0100

    change env variable

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
+
 const http = require('http');
 const mongoose = require('mongoose');
 const routeHandler = require('./src/routehandler');
 require('dotenv').config();
+
 
 
 const server = http.createServer((req, res) => {
@@ -10,14 +13,15 @@ const server = http.createServer((req, res) => {
 
 
 // Database connection
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI
 const PORT = process.env.PORT || 5000;
+console.log(uri, PORT)
 
 async function connectDB() {
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    server.listen(PORT, () => console.log(`Listening for requests on port ${PORT}`));
-    console.log('Connected to database');
+      await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+      server.listen(PORT, () => console.log(`Listening for requests on port ${PORT}`));
+      console.log('Connected to database');
 
   } catch (error) {
     console.log(`Failed to connect to database. ${error}`);
